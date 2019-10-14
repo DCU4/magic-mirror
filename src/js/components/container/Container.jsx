4fe910fed8fed1f44fc8b3f87f11a65060ef3707
Dylan Connor <DylanJConnor4@gmail.com>
2019-10-14 16:50:25 -0400

    add Time component

--- src/js/components/container/Container.jsx
+++ src/js/components/container/Container.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 // import Folder from "../container/Folder.jsx;"
 import Calendar from "../presentational/Calendar.jsx";
 import Weather from "../presentational/Weather.jsx";
+import Time from "../presentational/Time.jsx";
 
 class Container extends Component {
   constructor(props) {
@@ -20,6 +21,7 @@ class Container extends Component {
           {/* im thinking like, calendar and weather on top */}
           
           <Weather />
+          <Time />
           <Calendar />
         {/* // </header> */}
 
---
+++ src/js/components/presentational/Time.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+export default class Time extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     date: new Date()
+    }
+  }
+  
+  
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  render() {
+    // var checkTime = (i) => {
+    //   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    //   return i;
+    // }
+    // var h = this.state.hours;
+    // var m = this.state.minutes;
+    // var s = this.state.seconds;
+    // // m = checkTime(m);
+    // // s = checkTime(s);
+    // let date = new Date();
+    // console.log(date.toTimeString())
+    // var time = date.getHours() + ":" + date.getMinutes();
+    // console.log(time);
+    // console.log(h, m, s);
+
+    
+    return (
+      <section className="time">
+        <p id="time-stamp">{this.state.date.toLocaleTimeString()}</p>
+      </section>
+
+    );
+  }
+}
